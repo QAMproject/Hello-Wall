6146ca997e782453e70305e66a218981ce48ed0a
mquintela65 <mquintela65@gmail.com>
2018-07-28 15:20:43 -0500

    .

--- Hello Wall/Primary.cpp
+++ Hello Wall/Primary.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
+#include <iostream> 
+#include <string>
+
 
 int main()
 {
 	std::cout << "Hello Wall!" << std::endl;
+	std::string name;
+	std::cin >> name;
+	std::cout << "Hello " << name << std::endl;
 	system("pause");
 	return 0;
 }
